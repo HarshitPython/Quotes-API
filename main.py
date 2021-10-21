49c2d9d6251d33d551db955e206798f1be9fcd2c
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-21 10:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def fit_fontsize( n_chars):
         if n_chars > chars:
             return font_size
 
-# def get_quote():
+def get_quote():
 #     response = requests.get('https://api.kanye.rest/')
 #     response.raise_for_status()
 #     quote = response.json()['quote']
