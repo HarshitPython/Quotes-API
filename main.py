335259b9387bfb750c57dc6f9ee2eaf00322b811
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-10-23 21:21:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=25
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 # kanye_button.grid(row=1, column=0)
 
 
